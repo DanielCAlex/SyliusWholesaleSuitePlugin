b83d4c716e765152fcee80ffc5351bbebd4245ca
Daniel Alexandre <liengesbor@gmail.com>
2020-12-18 02:23:45 -0800

    Changed namespaces to SkyBoundTech/SyliusWholesaleSuitePlugin as per guide on https://docs.sylius.com/en/latest/book/plugins/guide/naming.html

--- src/AcmeSyliusExamplePlugin.php
+++ src/AcmeSyliusExamplePlugin.php
@@ -2,12 +2,12 @@
 
 declare(strict_types=1);
 
-namespace Acme\SyliusExamplePlugin;
+namespace SkyBoundTech\SyliusWholesaleSuitePlugin;
 
 use Sylius\Bundle\CoreBundle\Application\SyliusPluginTrait;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
-final class AcmeSyliusExamplePlugin extends Bundle
+final class SkyBoundTechSyliusWholesaleSuitePlugin extends Bundle
 {
     use SyliusPluginTrait;
 }
