7317a4905b710a6ddfcf43b59a4e66b66e254562
Daniel Alexandre <liengesbor@gmail.com>
2020-12-19 16:23:42 -0800

    Added the WholesaleRuleset.orm.xml mapping and its php file WholesaleRuleset.php.

---
+++ src/Entity/WholesaleRuleset.php
@@ -0,0 +1,117 @@
+<?php
+
+declare(strict_types=1);
+
+
+namespace SkyBoundTech\SyliusWholesaleSuitePlugin\Entity;
+
+use Sylius\Component\Resource\Model\ResourceInterface;
+
+class WholesaleRuleset implements ResourceInterface
+{
+    /** @var int */
+    protected $id;
+
+    /** @var string */
+    protected $name;
+
+    /** @var string */
+    protected $type;
+
+    /** @var string */
+    protected $scope;
+
+    /** @var string|null */
+    protected $description;
+
+    /** @var boolean */
+    protected $isEnabled;
+
+    /**
+     * @return int
+     */
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return string
+     */
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param string $name
+     */
+    public function setName(string $name): void
+    {
+        $this->name = $name;
+    }
+
+    /**
+     * @return string
+     */
+    public function getType(): string
+    {
+        return $this->type;
+    }
+
+    /**
+     * @param string $type
+     */
+    public function setType(string $type): void
+    {
+        $this->type = $type;
+    }
+
+    /**
+     * @return string
+     */
+    public function getScope(): string
+    {
+        return $this->scope;
+    }
+
+    /**
+     * @param string $scope
+     */
+    public function setScope(string $scope): void
+    {
+        $this->scope = $scope;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    /**
+     * @param string|null $description
+     */
+    public function setDescription(?string $description): void
+    {
+        $this->description = $description;
+    }
+
+    /**
+     * @return bool
+     */
+    public function isEnabled(): bool
+    {
+        return $this->isEnabled;
+    }
+
+    /**
+     * @param bool $isEnabled
+     */
+    public function setIsEnabled(bool $isEnabled): void
+    {
+        $this->isEnabled = $isEnabled;
+    }
+}
