2162b1dadc5066443de23177c09fb312c54d2c6c
Daniel Alexandre <liengesbor@gmail.com>
2020-12-19 22:25:25 -0800

    Added author and copyright comment blocks in file headers.

--- src/Entity/WholesaleRuleset.php
+++ src/Entity/WholesaleRuleset.php
@@ -1,7 +1,15 @@
 <?php
 
-declare(strict_types=1);
+/*
+ * This file was created by a developer at SkyBound Tech.
+ * @author Daniel Alexandre <daniel.alexandre@skyboundtech.co>
+ *
+ * (c) SkyBound Tech
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
 
+declare(strict_types=1);
 
 namespace SkyBoundTech\SyliusWholesaleSuitePlugin\Entity;
 
