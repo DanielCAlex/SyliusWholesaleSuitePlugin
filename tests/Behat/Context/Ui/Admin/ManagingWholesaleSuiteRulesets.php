09e2e99b67de8de7b572569ded4e29a8a1ebc13a
Daniel Alexandre <liengesbor@gmail.com>
2020-12-21 02:54:35 -0800

    Implemented theFollowingWholesaleRulesetsExist(). Added truncate tables "before suite" hook from TruncateContext.

--- tests/Behat/Context/Ui/Admin/ManagingWholesaleSuiteRulesets.php
+++ tests/Behat/Context/Ui/Admin/ManagingWholesaleSuiteRulesets.php
@@ -1,22 +1,98 @@
 <?php
+/*
+ * This file was created by a developer at SkyBound Tech.
+ * @author Daniel Alexandre <daniel.alexandre@skyboundtech.co>
+ *
+ * (c) SkyBound Tech
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
 
 declare(strict_types=1);
 
 namespace Tests\SkyBoundTech\SyliusWholesaleSuitePlugin\Behat\Context\Ui\Admin;
 
-use Behat\Gherkin\Node\TableNode;
+use Exception;
 use Behat\Behat\Context\Context;
+use Behat\Gherkin\Node\TableNode;
+use Doctrine\ORM\EntityManagerInterface;
 use Behat\Behat\Tester\Exception\PendingException;
+use SkyBoundTech\SyliusWholesaleSuitePlugin\Entity\WholesaleRuleset;
+use Tests\SkyBoundTech\SyliusWholesaleSuitePlugin\Behat\Context\Hooks\TruncateContext;
 
 final class ManagingWholesaleSuiteRulesets implements Context
 {
 
     /**
+     * @var TruncateContext
+     */
+    static $truncateContext;
+    /**
+     * @var EntityManagerInterface
+     */
+    protected $entityManager;
+
+    public function __construct(
+        EntityManagerInterface $entityManager,
+        TruncateContext $truncateContext
+    ) {
+        $this->entityManager = $entityManager;
+        self::$truncateContext = $truncateContext;
+    }
+
+    /**
+     * @BeforeSuite
+     * Truncate the table beforehand so we don't get unique constraint errors
+     */
+    public static function truncate()
+    {
+        $tablesToTruncate = [
+            0 => new WholesaleRuleset(),
+        ];
+        self::$truncateContext->truncateTables($tablesToTruncate);
+    }
+
+    /**
      * @Given the following wholesale rulesets exist
      */
-    public function theFollowingWholesaleRulesetsExist(TableNode $table)
+    public function theFollowingWholesaleRulesetsExist(TableNode $table): void
     {
-        throw new PendingException();
+        foreach ($table as $node) {
+            $ruleset = new WholesaleRuleset();
+
+            /** @var string $name */
+            $name = $node['name'];
+
+            /** @var string $type */
+            $type = $node['type'];
+
+            /** @var string $scope */
+            $scope = $node['scope'];
+
+            /** @var string $description */
+            $description = $node['description'];
+
+            /** @var boolean $isEnabled */
+            $isEnabled = filter_var($node['isEnabled'], FILTER_VALIDATE_BOOLEAN);
+
+            $ruleset->setName($name);
+            $ruleset->setType($type);
+            $ruleset->setScope($scope);
+            $ruleset->setDescription($description);
+            $ruleset->setIsEnabled($isEnabled);
+
+            $this->entityManager->persist($ruleset);
+        }
+        $this->entityManager->flush();
+
+        $rulesets = $this->entityManager
+            ->getRepository(WholesaleRuleset::class)
+            ->findAll()
+        ;
+
+        if (count($rulesets) !== 4 || $rulesets === null) {
+            throw new Exception('The rulesets were not created.');
+        }
     }
 
     /**
