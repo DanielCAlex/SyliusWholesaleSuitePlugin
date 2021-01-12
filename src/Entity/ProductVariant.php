10880b00c140e99d7ac4c6387d5215ef05eb2872
Daniel Alexandre <liengesbor@gmail.com>
2021-01-12 14:45:29 -0800

    Overrode sylius core ProductVariant entity. Gave custom entity ManyToMany relationship with WholesaleRuleset.

---
+++ src/Entity/ProductVariant.php
@@ -0,0 +1,27 @@
+<?php
+/*
+ * This file was created by a developer at SkyBound Tech.
+ * @author Daniel Alexandre <daniel.alexandre@skyboundtech.co>
+ *
+ * (c) SkyBound Tech
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace SkyBoundTech\SyliusWholesaleSuitePlugin\Entity;
+
+use Sylius\Component\Core\Model\Product as BaseProduct;
+use SkyBoundTech\SyliusWholesaleSuitePlugin\Traits\ProductVariantWholesaleRulesetTrait;
+
+class ProductVariant extends BaseProduct
+{
+    use ProductVariantWholesaleRulesetTrait;
+
+    public function __construct()
+    {
+        parent::__construct();
+        $this->initWholesaleRulesetTrait();
+    }
+}
