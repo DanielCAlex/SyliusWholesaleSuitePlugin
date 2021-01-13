95208ac8114d914f0faf14250fbce3df1a2d2eb0
Daniel Alexandre <liengesbor@gmail.com>
2021-01-13 12:55:30 -0800

    Overrode ProductVariantRepository.

---
+++ src/Repository/ProductVariantRepository.php
@@ -0,0 +1,19 @@
+<?php
+/*
+ * This file was created by a developer at SkyBound Tech.
+ * @author Daniel Alexandre <daniel.alexandre@skyboundtech.co>
+ *
+ * (c) SkyBound Tech
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace SkyBoundTech\SyliusWholesaleSuitePlugin\Repository;
+
+use Sylius\Bundle\CoreBundle\Doctrine\ORM\ProductVariantRepository as BaseProductVariantRepository;
+
+class ProductVariantRepository extends BaseProductVariantRepository
+{
+}
