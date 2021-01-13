38a855ada0ff8acaf6bf211014398d6418e50852
Daniel Alexandre <liengesbor@gmail.com>
2021-01-13 15:30:15 -0800

    Added findByNamePart

--- src/Repository/ProductVariantRepository.php
+++ src/Repository/ProductVariantRepository.php
@@ -16,4 +16,21 @@ use Sylius\Bundle\CoreBundle\Doctrine\ORM\ProductVariantRepository as BaseProduc
 
 class ProductVariantRepository extends BaseProductVariantRepository
 {
+    public function findByNamePart(string $phrase, string $locale, ?int $limit = null): array
+    {
+        return $this->createQueryBuilder('o')
+            ->innerJoin(
+                'o.translations',
+                'translation',
+                'WITH',
+                'translation.locale = :locale'
+            )
+            ->andWhere('translation.name LIKE :name')
+            ->setParameter('name', '%' . $phrase . '%')
+            ->setParameter('locale', $locale)
+            ->setMaxResults($limit)
+            ->getQuery()
+            ->getResult()
+            ;
+    }
 }
