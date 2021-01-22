05f458c2d53ebd10afbcf0fbc4805531a586d804
Daniel Alexandre <liengesbor@gmail.com>
2021-01-21 18:21:46 -0800

    Created ProductVariantInterface.

---
+++ src/Entity/ProductVariantInterface.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file was created by a developer at SkyBound Tech.
+ * @author Daniel Alexandre <daniel.alexandre@skyboundtech.co>
+ *
+ * (c) SkyBound Tech
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace SkyBoundTech\SyliusWholesaleSuitePlugin\Entity;
+
+interface ProductVariantInterface
+{
+}
