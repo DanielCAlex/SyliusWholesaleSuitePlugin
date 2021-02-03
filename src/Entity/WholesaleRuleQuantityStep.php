92d62a011ac980ad541ef37dc932e8793be0e340
Daniel Alexandre <liengesbor@gmail.com>
2021-02-03 13:44:50 -0800

    Made WholesaleRuleQuantityStep a resource.

--- src/Entity/WholesaleRuleQuantityStep.php
+++ src/Entity/WholesaleRuleQuantityStep.php
@@ -12,7 +12,9 @@ declare(strict_types=1);
 
 namespace SkyBoundTech\SyliusWholesaleSuitePlugin\Entity;
 
-class WholesaleRuleQuantityStep extends BaseEntity implements WholesaleRuleQuantityStepInterface
+use Sylius\Component\Resource\Model\ResourceInterface;
+
+class WholesaleRuleQuantityStep extends BaseEntity implements WholesaleRuleQuantityStepInterface, ResourceInterface
 {
     /** @var int */
     protected $id;
@@ -27,84 +29,53 @@ class WholesaleRuleQuantityStep extends BaseEntity implements WholesaleRuleQuant
     /** @var WholesaleRuleset */
     protected $ruleset;
 
-    /**
-     * @return int
-     */
     public function getId(): int
     {
         return $this->id;
     }
 
-    /**
-     * @param int $id
-     */
     public function setId(int $id): void
     {
         $this->id = $id;
     }
 
-    /**
-     * @return string
-     */
     public function getName(): string
     {
         return $this->name;
     }
 
-    /**
-     * @param string $name
-     */
     public function setName(string $name): void
     {
         $this->name = $name;
     }
 
-    /**
-     * @return string
-     */
     public function getDescription(): string
     {
         return $this->description;
     }
 
-    /**
-     * @param string $description
-     */
     public function setDescription(string $description): void
     {
         $this->description = $description;
     }
 
-    /**
-     * @return int
-     */
     public function getQuantityStep(): int
     {
         return $this->quantityStep;
     }
 
-    /**
-     * @param int $quantityStep
-     */
     public function setQuantityStep(int $quantityStep): void
     {
         $this->quantityStep = $quantityStep;
     }
 
-    /**
-     * @return bool
-     */
     public function isEnabled(): bool
     {
         return $this->enabled;
     }
 
-    /**
-     * @param bool $enabled
-     */
     public function setEnabled(bool $enabled): void
     {
         $this->enabled = $enabled;
     }
-
 }
