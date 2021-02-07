bb200420d3daf54aab381db605a862eb7ecee473
Daniel Alexandre <liengesbor@gmail.com>
2021-02-07 11:27:51 -0800

    Added docblocks.

--- src/Entity/WholesaleRuleQuantityStepInterface.php
+++ src/Entity/WholesaleRuleQuantityStepInterface.php
@@ -17,37 +17,89 @@ use Sylius\Component\Core\Model\TaxonInterface;
 
 interface WholesaleRuleQuantityStepInterface
 {
+    /**
+     * @return int
+     */
     public function getId(): int;
 
+    /**
+     * @param string $name
+     */
     public function setName(string $name): void;
 
+    /**
+     * @return string
+     */
     public function getName(): string;
 
+    /**
+     * @return string|null
+     */
     public function getDescription(): ?string;
 
+    /**
+     * @param string $description
+     */
     public function setDescription(string $description): void;
 
+    /**
+     * @return int
+     */
     public function getQuantityStep(): int;
 
+    /**
+     * @param int $quantityStep
+     */
     public function setQuantityStep(int $quantityStep): void;
 
+    /**
+     * @return string
+     */
     public function getScope(): string;
 
+    /**
+     * @param string $scope
+     */
     public function setScope(string $scope): void;
 
+    /**
+     * @param bool $enabledStatus
+     */
     public function setEnabled(bool $enabledStatus): void;
 
+    /**
+     * @return bool
+     */
     public function isEnabled(): bool;
 
+    /**
+     * @param WholesaleRulesetInterface $ruleset
+     */
     public function setRuleset(WholesaleRulesetInterface $ruleset): void;
 
+    /**
+     * @return WholesaleRulesetInterface
+     */
     public function getRuleset(): WholesaleRulesetInterface;
 
+    /**
+     * @return Collection|null
+     */
     public function getTaxons(): ?Collection;
 
+    /**
+     * @param TaxonInterface $taxon
+     */
     public function addTaxon(TaxonInterface $taxon): void;
 
+    /**
+     * @param TaxonInterface $taxon
+     */
     public function removeTaxon(TaxonInterface $taxon): void;
 
+    /**
+     * @param TaxonInterface $taxon
+     * @return bool
+     */
     public function hasTaxon(TaxonInterface $taxon): bool;
 }
