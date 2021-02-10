12952dcdcec8b47ee5447778c0d813c124d1ad79
Daniel Alexandre <liengesbor@gmail.com>
2021-02-09 21:15:57 -0800

    Added WholesaleRuleQuantityStepByProductType

---
+++ src/Form/Type/WholesaleRuleQuantityStepByProductType.php
@@ -0,0 +1,66 @@
+<?php
+/*
+ * This file was created by a developer at SkyBound Tech.
+ * @author Daniel Alexandre <daniel.alexandre@skyboundtech.co>
+ *
+ * (c) SkyBound Tech
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace SkyBoundTech\SyliusWholesaleSuitePlugin\Form\Type;
+
+use SkyBoundTech\SyliusWholesaleSuitePlugin\Entity\WholesaleRuleQuantityStep;
+use Sylius\Bundle\ProductBundle\Form\Type\ProductAutocompleteChoiceType;
+use Sylius\Bundle\ResourceBundle\Form\Type\AbstractResourceType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+final class WholesaleRuleQuantityStepByProductType extends AbstractResourceType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add(
+                'name',
+                TextType::class,
+                [
+                    'label' => 'skyboundtech_sylius_wholesale_suite_plugin.ui.wholesale_rules.rule_name',
+                ]
+            )
+            ->add(
+                'description',
+                TextareaType::class,
+                [
+                    'label' => 'skyboundtech_sylius_wholesale_suite_plugin.ui.wholesale_rules.rule_description',
+                ]
+            )
+            ->add(
+                'quantityStep',
+                IntegerType::class,
+                [
+                    'label' => 'skyboundtech_sylius_wholesale_suite_plugin.ui.wholesale_rules.quantity_step.quantity',
+                ]
+            )
+            ->add(
+                'products',
+                ProductAutocompleteChoiceType::class,
+                [
+                    'multiple' => true,
+                    'label' => 'skyboundtech_sylius_wholesale_suite_plugin.ui.wholesale_rules.quantity_step.select_products',
+                    'required' => true,
+                ]
+            )
+            ->add('enabled');
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults(['data_class' => WholesaleRuleQuantityStep::class]);
+    }
+}
--- src/Form/Type/WholesaleRulesetType.php
+++ src/Form/Type/WholesaleRulesetType.php
@@ -53,6 +53,16 @@ final class WholesaleRulesetType extends AbstractResourceType
                     'allow_delete' => true,
                     'by_reference' => false,
                 ]
+            )
+            ->add(
+                'quantityStepRulesByProduct',
+                CollectionType::class,
+                [
+                    'entry_type' => WholesaleRuleQuantityStepByProductType::class,
+                    'allow_add' => true,
+                    'allow_delete' => true,
+                    'by_reference' => false,
+                ]
             );
     }
 }
